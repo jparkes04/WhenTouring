6882ac17e797b4f271c19e14beb71257342a8936
inline03 <jparkes@outlook.com>
2025-09-09 13:39:28 +0100

    Add throttling to client apis with bottleneck

--- src/lib/clients/musicbrainz.ts
+++ src/lib/clients/musicbrainz.ts
@@ -1,10 +1,19 @@
+import Bottleneck from "bottleneck";
+
+const limiter = new Bottleneck({
+  maxConcurrent: 1,
+  minTime: 25
+});
+
 export async function searchMusicBrainzArtists(artistName: string) {
-  const res = await fetch(`https://musicbrainz.org/ws/2/artist?query=${artistName}&limit=1`, {
-    headers: {
-        "User-Agent":  `${process.env.APP_NAME}/${process.env.APP_VERSION} (${process.env.CONTACT_ADDRESS})`,
-        "Accept": "application/json"
-    },
-  });
+  const res = await limiter.schedule( () =>
+    fetch(`https://musicbrainz.org/ws/2/artist?query=${encodeURIComponent(artistName)}&limit=1`, {
+      headers: {
+          "User-Agent":  `${process.env.APP_NAME}/${process.env.APP_VERSION} (${process.env.CONTACT_ADDRESS})`,
+          "Accept": "application/json"
+      },
+    })
+  );
   if (!res.ok) throw new Error(`Find MBID error: ${res.status}`);
   return res.json();
 }
--- src/lib/clients/setlistfm.ts
+++ src/lib/clients/setlistfm.ts
@@ -1,10 +1,21 @@
+import Bottleneck from "bottleneck";
+
+const limiter = new Bottleneck({
+  maxConcurrent: 1,
+  minTime: 600
+});
+
 export async function fetchEventsFromSetlistfm(mbid: string, page: number) {
-  const res = await fetch(`https://api.setlist.fm/rest/1.0/artist/${mbid}/setlists?p=${page}`, {
-    headers: {
-        "x-api-key": `${process.env.SETLISTFM_API_KEY}`,
-        "Accept": "application/json"
-    },
-  });
+  const res = await limiter.schedule( () => 
+    fetch(`https://api.setlist.fm/rest/1.0/artist/${mbid}/setlists?p=${page}`, {
+      headers: {
+          "x-api-key": process.env.SETLISTFM_API_KEY!,
+          "Accept": "application/json"
+      },
+    })
+  );
+
   if (!res.ok) throw new Error(`Fetch from SetlistFM error: ${res.status}`);
+
   return res.json();
 }
