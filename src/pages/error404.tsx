14d98b56c4c332a49e5ea2644e594ab253c36ea7
inline03 <jparkes@outlook.com>
2025-09-11 15:21:13 +0100

    Create error pages

---
+++ src/pages/error404.tsx
@@ -0,0 +1,16 @@
+import HomeLink from "@/components/HomeLink";
+
+export default function Error429() {
+  return (
+    <main>
+      <h1 className="text-2xl lg:text-3xl mb-4">Artist Not Found</h1>
+
+      <p className="text-md lg:text-lg mb-5 lg:mb-7">
+        We were unable to find the artist that you searched for.
+      </p> 
+
+      <HomeLink />
+    </main>
+  )
+}
+
