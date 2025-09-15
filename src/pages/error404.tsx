699d6287ef3adae8bdd930b792b0c75c44d872eb
inline03 <jparkes@outlook.com>
2025-09-15 20:59:18 +0100

    Update error page UIs

--- src/pages/error404.tsx
+++ src/pages/error404.tsx
@@ -2,14 +2,22 @@ import InternalLink from "@/components/InternalLink";
 
 export default function Error429() {
   return (
-    <main>
-      <h1 className="text-2xl lg:text-3xl mb-4">Artist Not Found</h1>
+    <>
+        <div className="mt-10"></div>
+        <div className="flex justify-center">
+            <div className="bg-zinc-700 rounded-2xl mt-5 p-6 lg:p-10 w-full">
+                <h2 className="text-xl lg:text-3xl mb-4 lg:mb-7">
+                    Artist Not Found             
+                </h2>
 
-      <p className="text-md lg:text-lg mb-5 lg:mb-7">
-        We were unable to find the artist that you searched for.
-      </p> 
+                <p className="text-sm lg:text-lg mb-7 lg:mb-10">
+                  We were unable to find the artist that you searched for.
+                </p>
 
-      <InternalLink label="Return to Home" href="/"/>
-    </main>
+            <InternalLink label="Return to Home" href="/"/>
+            <div className="mb-2"></div>
+            </div>
+        </div>
+    </>
   )
 }
